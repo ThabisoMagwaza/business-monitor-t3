cb1426e3d26401f3fcd0ddf5e7c40ec9f0260c85
Thabiso Magwaza <magwazathabiso9@gmail.com>
2024-06-16 04:13:36 +0200

    feat: connect to database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
 import BusinessHealthSummary from '~/components/BusinessHealthSummary';
+import { db } from '~/server/db';
 
-export default function Home() {
+export default async function Home() {
+  const posts = await db.query.posts.findMany();
   return (
     <main>
+      <ul>
+        {posts.map((post, index) => (
+          <li key={index}>{post.name}</li>
+        ))}
+      </ul>
       <BusinessHealthSummary />
     </main>
   );
