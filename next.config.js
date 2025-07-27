b79a88b8d5aeca7101a8359158147c6a3a0fc9b4
Thabiso Magwaza <magwazathabiso9@gmail.com>
2025-07-27 19:15:03 +0200

    fix: increase server action body size limit

--- next.config.js
+++ next.config.js
@@ -15,6 +15,11 @@ const config = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '10mb',
+    },
+  },
 };
 
 export default config;
