57f0cb5cae701376ff68099f5c1faa646d7492b5
Thabiso Magwaza <magwazathabiso9@gmail.com>
2025-07-29 06:07:43 +0200

    feat: header done

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,31 @@
 import { getUserInfo } from '~/app/db-helpers';
-import HeaderStyled from './HeaderStyled';
+import Link from 'next/link';
+import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
 
 async function Header() {
   const user = await getUserInfo();
 
-  return <HeaderStyled isAdmin={user?.isAdmin ?? false} />;
+  const isAdmin = user?.isAdmin ?? false;
+
+  return (
+    <header className="border-b border-zinc-600">
+      <div className="flex justify-between py-3 my-0 mx-auto px-4 max-w-[calc(1000px+1rem)]">
+        <Link href="/" className="font-bold decoration-none text-inherit">
+          Business Monitor
+        </Link>
+
+        <div className="flex items-center gap-2">
+          <SignedOut>
+            <SignInButton>Sign In</SignInButton>
+          </SignedOut>
+          {isAdmin && <Link href="/add-users">Manage</Link>}
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
