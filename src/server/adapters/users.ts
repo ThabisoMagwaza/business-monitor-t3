e44347c55de232fddbd1a4f59a9c3a7dbdf57464
Thabiso Magwaza <magwazathabiso9@gmail.com>
2025-09-15 23:47:50 +0200

    fix: cache user

--- src/server/adapters/users.ts
+++ src/server/adapters/users.ts
@@ -2,13 +2,27 @@ import { type User, userSchema } from '~/lib/types/user';
 import { db } from '~/server/db';
 import { users } from '~/server/db/schema';
 import { eq } from 'drizzle-orm';
+import { unstable_cache } from 'next/cache';
 
-export const getUser = async (userId: string): Promise<User | undefined> => {
-  const user = await db.select().from(users).where(eq(users.id, userId));
+const getUserQuery = async (userId: string) =>
+  unstable_cache(
+    async (userId: string): Promise<User | undefined> => {
+      const user = await db.select().from(users).where(eq(users.id, userId));
 
-  if (user.length === 0) {
-    return; // User is not registered
-  }
+      if (user.length === 0) {
+        return; // User is not registered
+      }
 
-  return userSchema.parse(user[0]);
+      return userSchema.parse(user[0]);
+    },
+    [`userId-${userId}`],
+    { tags: ['user'] }
+  );
+
+export const getUser = async (userId: string) => {
+  const cachedUser = await getUserQuery(userId);
+
+  const user = await cachedUser(userId);
+
+  return user;
 };
